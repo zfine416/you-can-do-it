879d2b9d7377011ea16c9669d0b504f715cb0873
Zachary Fine <zfine416@gmail.com>
2019-01-31 21:28:24 -0500

    update images

--- src/components/common/dog.jsx
+++ src/components/common/dog.jsx
@@ -4,7 +4,13 @@ const Dog = props => {
   const { name } = props.breed;
   return (
     <div className="flex flex-col dog">
-      <img src={props.image()} alt="" />
+      <div
+        style={{
+          backgroundImage: `url(${props.image()})`
+        }}
+        className="dog-image"
+      />
+      {/* <img src={props.image()} alt="" /> */}
       <h4>{name}</h4>
     </div>
   );
